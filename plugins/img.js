dd54bf4d9b051ffe450ec20e2d6cb9a67f4fb0cb
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-05-04 22:40:41 +0530

    Create img.js

---
+++ plugins/img.js
@@ -0,0 +1,45 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')	
+
+
+
+
+cmd({
+    pattern: "img",
+    react: '👾',
+    desc: 'to down images',
+    category: "download",
+    use: '.img dark',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    
+    if (!q) throw `Example: ${prefix + command} Bike`
+
+    let gis = require('g-i-s')
+    gis(q, async (error, result) => {
+        if (error) {
+            console.error('Error fetching images:', error);
+		
+            return reply('Error fetching images. Please try again later.')
+        }
+
+        const topImages = result.slice(0, 5); // Extract top 5 images
+
+        for (let i = 0; i < topImages.length; i++) {
+            const imageUrl = topImages[i].url
+          let Message = {
+              image: { url: imageUrl },caption: `*-------「 IMAGE SEARCH 」-------*\n🤠*Image ${i + 1}`,
+           }
+
+//let senda = await conn.sendMessage(from, { document: {url: imageUrl },fileName: 'image' + '.jpg', mimetype: 'image/jpeg' ,caption: `*-------「 VAJIRA MD GIMAGE SEARCH 」-------*\n🤠 *Query* : ${q}\n\n🔗 *Image ${i + 1} Url* : ${imageUrl}`,}, { quoted: mek })  
+		
+            conn.sendMessage(from, Message, { quoted: mek })
+        }
+    })
+} catch (e) {
+l(e)
+}
+})
