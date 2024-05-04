f67fdf0874fef2b3f27481f9a60b4dd4c9faef63
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-05-04 21:49:42 +0530

    ＭＲ ＫＡＳＵＮ Ｖ2

--- config.js
+++ config.js
@@ -10,11 +10,11 @@ ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GR
 ONLY_ME: process.env.ONLY_ME === undefined ? 'true' : process.env.ONLY_ME,
 ADDRESSES: process.env.ADDRESSES === undefined ? '' : process.env.ADDRESSES,
 ALIVE: process.env.ALIVE === undefined ? `Hello im alive now !!` : process.env.ALIVE,
-OWNER: process.env.OWNER === undefined ? `94725881990` : process.env.OWNER,
+OWNER: process.env.OWNER === undefined ? `94772801923` : process.env.OWNER,
 PREFIX: process.env.PREFIX === undefined ? '@' : process.env.PREFIX,
-FOOTER: process.env.FOOTER=== undefined ? '‌ꜱᴀᴄʜɪʙᴏᴛ': process.env.FOOTER,
+FOOTER: process.env.FOOTER=== undefined ? '‌ᴍʀᴋᴀꜱᴜɴ': process.env.FOOTER,
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? false : process.env.ALWAYS_ONLINE,
 WAPRESENCE:process.env.WAPRESENCE === undefined ? false : process.env.WAPRESENCE, // 'composing' (typing) | 'recording' (recording) | 'paused'
-LOGO: process.env.LOGO === undefined ? `https://telegra.ph/file/98b9cdf149a52bef8f4d5.jpg` : process.env.LOGO
+LOGO: process.env.LOGO === undefined ? `https://telegra.ph/file/de725ab76fc5fbb31beee.jpg` : process.env.LOGO
 };
