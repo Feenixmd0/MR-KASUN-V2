ca81248f5a8b6f61b148a3ffc8bc4967012f3264
🇱🇰MR_KASUN-MD <168183976+Feenixmd0@users.noreply.github.com>
2024-05-05 12:56:42 +0530

    Create fb.js

---
+++ plugins/fb.js
@@ -0,0 +1,87 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')	
+
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+async function fbDownloader(url) {
+	try {
+		const response1 = await axios({
+			method: 'POST',
+			url: 'https://snapsave.app/action.php?lang=vn',
+			headers: {
+				"accept": "*/*",
+				"accept-language": "vi,en-US;q=0.9,en;q=0.8",
+				"content-type": "multipart/form-data",
+				"sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Microsoft Edge\";v=\"110\"",
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": "\"Windows\"",
+				"sec-fetch-dest": "empty",
+				"sec-fetch-mode": "cors",
+				"sec-fetch-site": "same-origin",
+				"Referer": "https://snapsave.app/vn",
+				"Referrer-Policy": "strict-origin-when-cross-origin"
+			},
+			data: {
+				url
+			}
+		});
+
+		let html;
+		const evalCode = response1.data.replace('return decodeURIComponent', 'html = decodeURIComponent');
+		eval(evalCode);
+		html = html.split('innerHTML = "')[1].split('";\n')[0].replace(/\\"/g, '"');
+
+		const $ = cheerio.load(html);
+		const download = [];
+
+		const tbody = $('table').find('tbody');
+		const trs = tbody.find('tr');
+
+		trs.each(function (i, elem) {
+			const trElement = $(elem);
+			const tds = trElement.children();
+			const quality = $(tds[0]).text().trim();
+			const url = $(tds[2]).children('a').attr('href');
+			if (url != undefined) {
+				download.push({
+					quality,
+					url
+				});
+			}
+		});
+
+		return {
+			success: true,
+			download
+		};
+	}
+	catch (err) {
+		return {
+			success: false
+		};
+	}
+}
+function fbreg(url) {
+const fbRegex = /(?:https?:\/\/)?(?:www\.)?(m\.facebook|facebook|fb)\.(com|me|watch)\/(?:(?:\w\.)*#!\/)?(?:groups\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/
+return fbRegex.test(url);
+}
+
+
+
+cmd({
+  pattern: "dvideo",
+  react: "🔓",
+  category: "download",
+  dontAddCommandList: true,
+  filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
+await conn.sendMessage(from, { video: { url: q }, caption: config.FOOTER}, { quoted: mek })
+await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
+} catch (e) {
+  reply('*ERROR !!*')
+l(e)
+}
+})
