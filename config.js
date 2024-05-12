33464d4ea44cd9f4bafab179c4880d5f205124c3
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-05-12 09:45:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ OWNER: process.env.OWNER === undefined ? `94772801923` : process.env.OWNER,
 PREFIX: process.env.PREFIX === undefined ? '@' : process.env.PREFIX,
 FOOTER: process.env.FOOTER=== undefined ? '‌ᴍʀᴋᴀꜱᴜɴ': process.env.FOOTER,
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? false : process.env.ALWAYS_ONLINE,
+AUTO_STATUS_READ: process.env.AUTO_STATUS_READ === undefined ? false : process.env.AUTO_STATUS_READ,
 WAPRESENCE:process.env.WAPRESENCE === undefined ? false : process.env.WAPRESENCE, // 'composing' (typing) | 'recording' (recording) | 'paused'
 LOGO: process.env.LOGO === undefined ? `https://telegra.ph/file/de725ab76fc5fbb31beee.jpg` : process.env.LOGO
 };
