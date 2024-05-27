434ce53be23ffe02163385083fc15596c414d753
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-05-27 11:14:14 +0530

    Update convert.js

--- plugins/convert.js
+++ plugins/convert.js
@@ -8,6 +8,8 @@ const { tmpdir } = require("os")
 const Crypto = require("crypto")
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg')
+let { img2url } = require('@blackamda/telegram-image-url');
+const fileType = require("file-type");
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 
@@ -217,4 +219,31 @@ l(e)
 }
 })
 
-     
+ cmd({
+    pattern: "img2url",
+    react: "🔗",
+    alias: ["tourl","imgurl","telegraph","imgtourl"],
+    desc: "to convert image to url",
+    category: "convert",
+    use: '.img2url <reply image>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try{
+    const isQuotedViewOnce = m.quoted ? (m.quoted.type === 'viewOnceMessage') : false
+    const isQuotedImage = m.quoted ? ((m.quoted.type === 'imageMessage') || (isQuotedViewOnce ? (m.quoted.msg.type === 'imageMessage') : false)) : false
+    if ((m.type === 'imageMessage') || isQuotedImage) {
+const fileType = require("file-type");
+  var nameJpg = getRandom('');
+  let buff = isQuotedImage ? await m.quoted.download(nameJpg) : await m.download(nameJpg)
+  let type = await fileType.fromBuffer(buff);
+  await fs.promises.writeFile("./" + type.ext, buff);
+  img2url("./" + type.ext).then(async url => {
+    await reply('\n' + url + '\n');
+});
+    } else return reply()
+} catch (e) {
+  reply();
+  l(e);
+}
+})    
